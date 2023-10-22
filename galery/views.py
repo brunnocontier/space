15ea5e4f7b84b231fbc0fd06d7706fdfa8c10205
Brunno Contier <brunnocontier@outlook.com>
2023-10-22 17:13:58 -0300

    migrations

--- galery/views.py
+++ galery/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galery/index.html')
+
+    dados = {
+        1: {'nome': 'Nebulosa de Carina', 'fonte': 'webbtelescope.org - NASA - JAMES WEBB'},
+        2: {'nome': 'Galaáxia NGC 1079', 'fonte': 'nasa.org - NASA - HUBBLE'},
+        3: {'nome': 'Galaáxia NGC 1079', 'fonte': 'nasa.org - NASA - HUBBLE'}
+    }
+    return render(request, 'galery/index.html', {'cards': dados})
 
 
 def image(request):
